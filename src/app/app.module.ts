18c9e4e7532d785d453dcc211fb8865f26d7ed0d
Paul Ngisiro <jngisiro@gmail.com>
2020-05-17 23:29:08 +0300

    Login and User dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './dashboard/user/user.component';
+import { FinanceComponent } from './dashboard/finance/finance.component';
+import { ManagerComponent } from './dashboard/manager/manager.component';
+import { AccountsComponent } from './dashboard/accounts/accounts.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserComponent,
+    FinanceComponent,
+    ManagerComponent,
+    AccountsComponent,
+    NavigationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
