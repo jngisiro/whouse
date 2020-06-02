f946acac227c74e3d3d55065e05b2a87beb48949
Paul Ngisiro <jngisiro@gmail.com>
2020-06-02 12:21:27 +0300

    Feat: Added time elapse from delivery

--- src/app/dashboard/transaction-details/transaction-details.component.ts
+++ src/app/dashboard/transaction-details/transaction-details.component.ts
@@ -3,6 +3,7 @@ import { Transaction } from 'src/app/models/transaction.model';
 import { DataService } from 'src/app/services/data.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { duration } from 'moment';
 
 @Component({
   selector: 'app-transaction-details',
@@ -16,7 +17,7 @@ export class TransactionDetailsComponent implements OnInit {
   loading: boolean = false;
   error: boolean = false;
   canEdit: boolean = false;
-
+  timeElapsed;
   constructor(
     private ds: DataService,
     private route: ActivatedRoute,
@@ -25,7 +26,7 @@ export class TransactionDetailsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.auth.user.subscribe(user => {
+    this.auth.user.subscribe((user) => {
       this.user = user;
     });
     this.loading = true;
@@ -33,10 +34,17 @@ export class TransactionDetailsComponent implements OnInit {
       this.ds.getTransactionById(params['id']).subscribe(
         (response: any) => {
           this.transaction = response.data.transaction;
-          if (this.user.role === 'user' && this.transaction.step === 'submitted'){
+          setInterval(() => {
+            this.timeElapsed = this.getTime();
+          }, 1000);
+          if (
+            this.user.role === 'user' &&
+            this.transaction.step === 'submitted'
+          ) {
             this.canEdit = true;
           }
           this.loading = false;
+          this.getTime();
         },
         (err) => {
           console.log(err);
@@ -46,7 +54,22 @@ export class TransactionDetailsComponent implements OnInit {
     });
   }
 
-  onEdit(){
-    this.router.navigate(['edit'], {queryParams: { canEdit: true, id: this.transaction._id}})
+  onEdit() {
+    this.router.navigate(['edit'], {
+      queryParams: { canEdit: true, id: this.transaction._id },
+    });
+  }
+
+  getTime() {
+    const today = new Date().getTime();
+    const deliveryDate = new Date(this.transaction.deliveryDate).getTime();
+
+    const difference = today - deliveryDate;
+    const seconds = duration(difference).seconds();
+    const minutes = duration(difference).minutes();
+    const hours = duration(difference).hours();
+    const days = duration(difference).days();
+
+    return `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
   }
 }
